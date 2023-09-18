ecf4c9799aeb103bb0deeebed459b97ffdc213b6
LesleyShonz <107910296+LesleyShonz@users.noreply.github.com>
2023-09-19 01:15:35 +0200

    Modified Polls

--- src/polls/MainPoll.js
+++ src/polls/MainPoll.js
@@ -1,54 +1,62 @@
-import './MainPoll.css';
-import reject3 from '../icons/reject3.svg'
-import React, { useState } from 'react';
-import CreatePollForm from './CreatePoll';
-import VoteForm from './VoteForm';
+import "./MainPoll.css";
+import reject3 from "../icons/reject3.svg";
+import React, { useState } from "react";
+import CreatePollForm from "./CreatePoll";
+import VoteForm from "./VoteForm";
 
 function MainPoll() {
-    const [isContainerVisible, setIsContainerVisible] = useState(true);
-    const [showCreateForm, setShowCreateForm] = useState(false);
-    const [showVoteForm, setShowVoteForm] = useState(false);
+  const [isContainerVisible, setIsContainerVisible] = useState(true);
+  const [showCreateForm, setShowCreateForm] = useState(false);
+  const [showVoteForm, setShowVoteForm] = useState(false);
 
-    const handleImageClick = () => {
-        setIsContainerVisible(false);
-    };
+  const handleImageClick = () => {
+    setIsContainerVisible(false);
+  };
 
-    const handleCreateClick = () => {
-        setShowCreateForm(true);
-        setShowVoteForm(false);
-        setIsContainerVisible(false);
-    };
+  const handleCreateClick = () => {
+    setShowCreateForm(true);
+    setShowVoteForm(false);
+    setIsContainerVisible(false);
+  };
 
-    const handleVoteClick = () => {
-        setShowCreateForm(false);
-        setShowVoteForm(true);
-        setIsContainerVisible(false);
-    };
+  const handleVoteClick = () => {
+    setShowCreateForm(false);
+    setShowVoteForm(true);
+    setIsContainerVisible(false);
+  };
 
-    return (
-        <div>
-            {isContainerVisible && (
-                <div className='createButton-container'>
-                    <img
-                        className='exit-poll1'
-                        src={reject3}
-                        alt="Click to hide container"
-                        onClick={handleImageClick}
-                    />
-                    <h1 className='header-create-poll'>I want to...</h1>
-                    <button className='create-new-poll-button' type='button' onClick={handleCreateClick}>
-                        Create new Poll
-                    </button>
-                    <button className='vote-on-existing-poll' type='button' onClick={handleVoteClick}>
-                        Vote on an existing poll
-                    </button>
-                </div>
-            )}
-            
-            {showCreateForm && <CreatePollForm />}
-            {showVoteForm && <VoteForm />}
+  return (
+    <div>
+      {isContainerVisible && (
+        <div className="createButton-container">
+          <img
+            className="exit-poll1"
+            src={reject3}
+            alt="Click to hide container"
+            onClick={handleImageClick}
+          />
+          <h1 className="header-create-poll">I want to...</h1>
+          <button
+            className="create-new-poll-button"
+            type="button"
+            onClick={handleCreateClick}
+          >
+            Create new Poll
+          </button>
+          <button
+            className="vote-on-existing-poll"
+            type="button"
+            onClick={handleVoteClick}
+          >
+            Vote on an existing poll
+          </button>
         </div>
-    );
-};
+      )}
+
+      {showCreateForm && <CreatePollForm />}
+      {showVoteForm && <VoteForm />}
+    </div>
+  );
+}
 
 export default MainPoll;
