c411b97c1ac929ab83886005ea4b14c17bf6fedd
LesleyShonz <107910296+LesleyShonz@users.noreply.github.com>
2023-09-21 22:28:58 +0200

    Update UserContext.js

--- src/Whiteboard/UserContext.js
+++ src/Whiteboard/UserContext.js
@@ -4,18 +4,6 @@
  * Provides a way to pass user data down the component tree without having to
  * pass props down manually at every level. Can be used with React's Context API.
  *
- * Typical usage:
- *
- * ```javascript
- * const user = useContext(UserContext);
- * ```
- *
- * Components that are wrapped inside the `UserContext.Provider` will have access
- * to the `value` prop provided to the `UserContext.Provider`. This allows for
- * easier state management and prop distribution throughout an application.
- *
- * @see https://reactjs.org/docs/context.html for more details on Context API.
- *
  * @component
  */
 import React from "react";
